851b55f4eb237270090d70c703369a8aaed16098
Todor Krumov <baito6u@gmail.com>
2024-07-18 06:48:09 +0300

    Update Navigation.jsx

--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -8,13 +8,11 @@ function Navigation() {
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/catalog">Catalog</Link></li>
-
         <div id="user">
           <li><Link to="/create">Add Product</Link></li>
           <li><Link to="/cart">My Cart</Link></li>
           <li><Link to="/logout">Logout</Link></li>
         </div>
-
         <div id="guest">
           <li><Link to="/login">Login</Link></li>
           <li><Link to="/register">Register</Link></li>
