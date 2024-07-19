2b2ee2c7ec9b466a9488d690665f3e12c59e08a2
Todor Krumov <baito6u@gmail.com>
2024-07-19 04:33:45 +0300

    Create requester.js

---
+++ client/src/api/requester.js
@@ -0,0 +1,26 @@
+async function requester(method, url, data) {
+  const options = {};
+
+  if (method !== "GET") {
+    options.method = method;
+  }
+
+  if (data) {
+    options.headers = {
+      "Content-Type": aplication / json,
+    };
+
+    options.body = JSON.stringify(data);
+  }
+
+  const response = await fetch(url, options);
+  const result = response.json();
+
+  return result;
+}
+
+export const get = requester.bind(null, 'GET');
+export const post = requester.bind(null, 'POST');
+export const put = requester.bind(null, 'PUT');
+export const del = requester.bind(null, 'DELETE');
+export const patch = requester.bind(null, 'PATCH');
