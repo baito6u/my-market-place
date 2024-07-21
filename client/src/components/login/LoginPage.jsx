8bd08bd0739b5a4b7cc5cb4d7190932fbac6cabc
Todor Krumov <baito6u@gmail.com>
2024-07-21 13:16:54 +0300

    change login to controlled form

--- client/src/components/login/LoginPage.jsx
+++ client/src/components/login/LoginPage.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
-import styles from './LoginPage.module.css';
+import React from "react";
+import useForm from "../../hooks/useForm";
+
+import styles from "./LoginPage.module.css";
 
 function LoginPage() {
+  const { values, onChange, onSubmit } = useForm({
+    email: "",
+    password: "",
+  });
+
   return (
     <div className={styles.login}>
       <h2>Login</h2>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className={styles.formGroup}>
-          <label>Email:</label>
-          <input type="email" />
+          <label htmlFor="email">Email:</label>
+          <input 
+          type="email" 
+          id="email" 
+          name="email" 
+          onChange={onChange} 
+          value={values.email}
+          />
+
         </div>
         <div className={styles.formGroup}>
-          <label>Password:</label>
-          <input type="password" />
+          <label htmlFor="password">Password:</label>
+          <input
+          va
+            type="password"
+            id="password"
+            name="password"
+            onChange={onChange}
+            value={values.password}
+          />
         </div>
         <button type="submit">Login</button>
       </form>
