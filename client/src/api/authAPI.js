b96ad5d342c5b38dc838ffa3903b9817c2d588d6
Todor Krumov <baito6u@gmail.com>
2024-07-21 16:32:30 +0300

    add registration

--- client/src/api/authAPI.js
+++ client/src/api/authAPI.js
@@ -3,7 +3,17 @@ import * as requester from "./requester";
 const BASE_URL = "http://localhost:3030/users";
 
 export const login = async (email, password) => {
-  const result = requester.post(`${BASE_URL}/login`, {
+  const result = await requester.post(`${BASE_URL}/login`, {
+    email,
+    password,
+  });
+
+  return result;
+};
+
+export const register = (username, email, password) => {
+  const result = requester.post(`${BASE_URL}/register`, {
+    username,
     email,
     password,
   });
