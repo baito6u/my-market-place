90d2b7379e74c70fc7ec0b277d1cd9364eba1824
Todor Krumov <baito6u@gmail.com>
2024-07-21 15:37:29 +0300

    add isAuth navigation show
    change navigation styles

--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -1,22 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Navigation.module.css';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./Navigation.module.css";
+
+import AuthContext from "../../contexts/authContext";
 
 function Navigation() {
+  const { isAuthenticated, username } = useContext(AuthContext);
+
   return (
     <nav className={styles.nav}>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/catalog">Catalog</Link></li>
-        <div id="user">
-          <li><Link to="/create">Add Product</Link></li>
-          <li><Link to="/cart">My Cart</Link></li>
-          <li><Link to="/logout">Logout</Link></li>
-        </div>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/catalog">Catalog</Link>
+        </li>
+        {isAuthenticated && (
+          <div id="user">
+            <span> | {username}'s profile |</span>
+            <li>
+              <Link to="/create">Add Product</Link>
+            </li>
+            <li>
+              <Link to="/cart">My Cart</Link>
+            </li>
+            <li>
+              <Link to="/logout">Logout</Link>
+            </li>
+          </div>
+        )}
+
+        {!isAuthenticated && (
         <div id="guest">
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/register">Register</Link></li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
         </div>
+        )}
       </ul>
     </nav>
   );
