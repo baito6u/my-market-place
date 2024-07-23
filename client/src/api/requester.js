6d86e5bca29dc49a8aa741aa77b5dded9957a41a
Todor Krumov <baito6u@gmail.com>
2024-07-23 05:37:11 +0300

    Update requester.js

--- client/src/api/requester.js
+++ client/src/api/requester.js
@@ -1,27 +1,45 @@
-async function requester(method, url, data) {
+const bildOption = (data) => {
   const options = {};
-
-  if (method !== "GET") {
-    options.method = method;
-  }
-
+  
   if (data) {
     options.body = JSON.stringify(data);
     options.headers = {
-      "Content-Type": "aplication/json"
+        'content-type': 'application/json'
     };
+}
+  const token = localStorage.getItem("accessToken");
 
-    options.body = JSON.stringify(data);
+  if (token) {
+    options.headers = {
+      ...options.headers,
+      "X-Authorization": token,
+    };
   }
 
-  const response = await fetch(url, options);
-  const result = response.json();
+  return options;
+};
+
+async function requester(method, url, data) {
+  const response = await fetch(url, {
+    ...bildOption(data),
+    method,
+  });
+
+  if (response.status === 204) {
+    return {};
+  }
+
+  const result = await response.json();
+
+  if (!response.ok) {
+    throw result;
+  }
 
   return result;
 }
 
-export const get = requester.bind(null, 'GET');
-export const post = requester.bind(null, 'POST');
-export const put = requester.bind(null, 'PUT');
-export const del = requester.bind(null, 'DELETE');
-export const patch = requester.bind(null, 'PATCH');
+export const get = requester.bind(null, "GET");
+export const post = requester.bind(null, "POST");
+export const put = requester.bind(null, "PUT");
+export const del = requester.bind(null, "DELETE");
+export const patch = requester.bind(null, "PATCH");
