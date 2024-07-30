cd511fe37f5f5398f259994db5342aa60fc1c3dc
Todor Krumov <baito6u@gmail.com>
2024-07-30 04:17:54 +0300

    change styles to latest products

--- client/src/components/home/latestProducts/LatestProduct.jsx
+++ client/src/components/home/latestProducts/LatestProduct.jsx
@@ -1,17 +1,19 @@
 import { Link } from "react-router-dom";
-
+import styles from "./LatestProducts.module.css";
 
 function LatestProduct({ _id, title, price, imageUrl }) {
   return (
-    <div >
-      <div>
+    <div className={styles.product}>
+      <div className={styles.imageWrap}>
         <img src={imageUrl} />
+      </div>
         <h2>Title: {title}</h2>
         <h6>price: $ {price}</h6>
-        <Link to={`/catalog/${_id}/details`} className="details-button">
-          Details
-        </Link>
-      </div>
+        <div className={styles.btn}>
+          <Link to={`/catalog/${_id}/details`} className="details-button">
+            Details
+          </Link>
+        </div>
     </div>
   );
 }
