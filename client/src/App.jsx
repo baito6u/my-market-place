7d471d3ce965e19b497e8fcb7785c14ca60a72a6
Todor Krumov <baito6u@gmail.com>
2024-07-31 20:54:58 +0300

    add cartProvider in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,33 +15,42 @@ import LogoutPage from "./components/logout/LogoutPage";
 import EditProductPage from "./components/edit/EditProductPage";
 import ErrorBoundary from "./components/ErrorBoundary";
 import AuthGuard from "./components/guards/AuthGuard";
+import { CartProvider } from "./contexts/cartContext";
 
 function App() {
   return (
     <ErrorBoundary>
       <AuthProvider>
-        <div className="wrapper">
-          <Navigation />
+        <CartProvider>
+          <div className="wrapper">
+            <Navigation />
 
-          <main className="box">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/catalog" element={<CatalogPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/catalog/:productId/details" element={<DetailsPage />}/>
-              
-              <Route element={<AuthGuard />}>
-                <Route path="/create" element={<CreateProduct />} />
-                <Route path="/catalog/:productId/edit" element={<EditProductPage />}/>
-                <Route path="/cart" element={<MyCartPage />} />
-                <Route path="/logout" element={<LogoutPage />} />
+            <main className="box">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/catalog" element={<CatalogPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route
+                  path="/catalog/:productId/details"
+                  element={<DetailsPage />}
+                />
+
+                <Route element={<AuthGuard />}>
+                  <Route path="/create" element={<CreateProduct />} />
+                  <Route
+                    path="/catalog/:productId/edit"
+                    element={<EditProductPage />}
+                  />
+                  <Route path="/cart" element={<MyCartPage />} />
+                  <Route path="/logout" element={<LogoutPage />} />
                 </Route>
-            </Routes>
-          </main>
+              </Routes>
+            </main>
 
-          <Footer />
-        </div>
+            <Footer />
+          </div>
+          </CartProvider>
       </AuthProvider>
     </ErrorBoundary>
   );
