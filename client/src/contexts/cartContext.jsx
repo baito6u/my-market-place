d700c2a3b6261ec2bafd6d45dcf9986d69debb0c
Todor Krumov <baito6u@gmail.com>
2024-07-31 20:53:35 +0300

    Create cartContext.jsx

---
+++ client/src/contexts/cartContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((prevCart) => [...prevCart, product]);
+  };
+
+  const removeFromCart = (productId) => {
+    setCart((prevCart) => prevCart.filter(item => item._id !== productId));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartContext;
