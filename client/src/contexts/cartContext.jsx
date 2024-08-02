611ef9f9966f3c8dff284ee72c006c3956977242
Todor Krumov <baito6u@gmail.com>
2024-08-02 05:13:03 +0300

    Update cartContext.jsx

--- client/src/contexts/cartContext.jsx
+++ client/src/contexts/cartContext.jsx
@@ -3,21 +3,26 @@ import { createContext, useState } from "react";
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
   const addToCart = (product) => {
-    setCart((prevCart) => [...prevCart, product]);
+    setCartItems((prevItems) => [...prevItems, product]);
   };
 
   const removeFromCart = (productId) => {
-    setCart((prevCart) => prevCart.filter(item => item._id !== productId));
+    setCartItems((prevItems) => prevItems.filter(item => item._id !== productId));
+  };
+
+  const values = {
+    cartItems,
+    addToCart,
+    removeFromCart,
   };
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
-      {children}
-    </CartContext.Provider>
+    <CartContext.Provider value={values}>{children}</CartContext.Provider>
   );
 };
 
 export default CartContext;
+
