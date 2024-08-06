89282a8f2008a4d6a628160edcd4c762e85a7560
Todor Krumov <baito6u@gmail.com>
2024-08-06 16:36:02 +0300

    Update index.js

--- server/functions/index.js
+++ server/functions/index.js
@@ -10,8 +10,8 @@ const server = require("./server");
  */
 
 const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 const { setGlobalOptions } = require("firebase-functions/v2");
+const logger = require("firebase-functions/logger");
 
 setGlobalOptions({
     region: "europe-west1"
